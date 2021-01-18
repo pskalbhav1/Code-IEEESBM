0a9ebc1665911e7fc912a7576c24ed7c22ccdf81
ankitmishra1201 <75842497+ankitmishra1201@users.noreply.github.com>
2021-01-18 12:07:52 +0530

    Add files via upload

---
+++ E-5.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b,c;
+    cout<<"enter first number";
+    cin>>a;
+    cout<<"enter second number";
+    cin>>b;
+    cout<<"enter third number";
+    cin>>c;2
+    if(a>b && a>c)
+    cout<<"the largest number is "<<a;
+    if(b>a && b>c)
+    cout<<"the largest number is"<<b;
+    if(c>a && c>b)
+    cout<<"the largest number is"<<c;
+    return 0; 
+
+}
