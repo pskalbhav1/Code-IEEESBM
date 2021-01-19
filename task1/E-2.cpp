22281911ef707b36bb86c176b466cfd46d0a9868
ayush2003shukla <77489209+ayush2003shukla@users.noreply.github.com>
2021-01-19 12:35:19 +0530

    Task 1 uploaded.

---
+++ task1/E-2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+int l;
+int b;
+cout<<"Enter length :"; cin >> l;
+cout<<"Enter breadth :"; cin >> b;
+cout << "Area is " << l*b;
+return 0;
+}
---
+++ task1/E-3.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+int a;
+int b;
+cout<<"Enter number 1 :"; cin >> a;
+cout<<"Enter number 2 :"; cin >> b;
+cout <<"The sum is " << a+b <<"\n" ;
+cout <<"The difference is " << a-b<<"\n";
+cout <<"The product is " << a*b<<"\n";
+cout <<"The quotient  is " << a/b<<"\n";
+return 0;
+}
