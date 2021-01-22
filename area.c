711413b6c3f0af234a82ff2639e2dd486f656db1
mridulgupta321 <77565263+mridulgupta321@users.noreply.github.com>
2021-01-22 21:48:50 +0530

    Add files via upload

---
+++ area.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int l=0,b=0,ar=0;
+    printf("Enter values of l and b \n");
+    scanf("%d %d",&l, &b);
+    ar=l*b;
+    printf("Area=");
+    printf("%d",ar);
+    return 0;
+}
+
+
+
+
+
+
+
+
