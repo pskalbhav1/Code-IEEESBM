984adf7f672b67ace393ea7d5453fe82334d7f84
kavya goel <kavyagoel@kavyas-MacBook-Air.local>
2021-01-22 13:25:19 +0530

    Task2

---
+++ E2_cpp.c
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+	cout<<"enter two numbers: ";
+	int a,b,c;
+	cout<<"a= ";
+	cin>>a;
+	cout<<"b= ";
+	cin>>b;
+	c=a;
+	a=b;
+	b=c;
+	cout<<"after sawpping a= "<<a<<"b= "<<b;
+
+	return 0;
+
+}
