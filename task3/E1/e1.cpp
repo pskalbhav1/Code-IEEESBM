780671942a4017060b0c10de5581a8e32b5302ae
ankitmishra1201 <75842497+ankitmishra1201@users.noreply.github.com>
2021-01-25 19:48:45 +0530

    Add files via upload

---
+++ task3/E1/e1.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<stdio.h>
+using namespace std;
+int main()
+
+{
+int n,i;
+cout<<"enter the number";
+cin>>n;
+
+for(i=1; i<=n; i++)
+{
+    if(i%2==0)
+    {
+    cout<<i<<" is the even numbers\n";
+    
+    
+    }
+
+}
+return 0;
+}
