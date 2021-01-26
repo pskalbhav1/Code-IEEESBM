828e492a1e4210dae105b9aa56d82ce9064b34c3
harshikasofat14 <75217296+harshikasofat14@users.noreply.github.com>
2021-01-26 19:10:24 +0700

    Update E-4.cpp

--- TASK 2/E-4.cpp
+++ TASK 2/E-4.cpp
@@ -1,28 +1,30 @@
-#include <iostream>  
-using namespace std;  
+#include <bits/stdc++.h>
+using namespace std;
 
-int main()
+int multiple(int n)
 {
-   int rem, rnum, num, sum, n;
-   cin>>num;
-   n=num;
-   while(num != 0){
-    rnum = num%10;
-    num = num/10;
-    //quo= n/10;
-    //rem = n - (quo * 10);
-    sum += rnum;
-    }
-    if(n%5 ==0 && sum%3 ==0){
+    if(n%5 ==0 && n%3 ==0){
         cout>>"\n\nThe number is multiple of both 3 and 5";
     }
-    else if(sum%3 == 0){
+    else if(n%3 == 0){
         cout>>"\nthe number is multiple of 3";
     }
     else if(n%5 == 0){
         cout>>"\nthe number is multiple of 5";
     }
-    else{cout>>"\nthis is not multiple of either 3 or 5";}
-    cout>>"\n\n";
+    else
+    {
+        cout>>"\nthis is not multiple of either 3 or 5";
+        
+    }
+ 
+}
+int main()
+{
+   int N;
+   cout>>"Enter the number:";
+   cin<<N;
+   cout>>multiple(N);
+   
    return 0;
 }
