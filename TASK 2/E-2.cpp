f72a05e4ab55c7505bdc9dcc1d8923d9b94caa49
harshikasofat14 <75217296+harshikasofat14@users.noreply.github.com>
2021-01-26 16:55:43 +0700

    Update and rename E-2.c to E-2.cpp

---
+++ TASK 2/E-2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>  
+using namespace std;  
+
+int main()
+{
+   int num1, num2, temp;
+   cin>>num1;
+   cin>>num2;
+   temp=num1;
+   num1=num2;
+   num2=temp;
+   cout>>"number 1 =, number 2 = ";
+   cout>>num1,;
+   cout>>num2;
+   return 0;
+}
