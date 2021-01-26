0de8e42fbdb46958b2fcd8af5ef63ff00ee559ec
harshikasofat14 <75217296+harshikasofat14@users.noreply.github.com>
2021-01-26 18:21:54 +0700

    Update E-2.cpp

--- TASK 2/E-2.cpp
+++ TASK 2/E-2.cpp
@@ -1,16 +1,20 @@
-#include <iostream>  
-using namespace std;  
+#include <iostream>
+using namespace std;
 
 int main()
 {
-   int num1, num2, temp;
-   cin>>num1;
-   cin>>num2;
-   temp=num1;
-   num1=num2;
-   num2=temp;
-   cout>>"number 1 =, number 2 = ";
-   cout>>num1,;
-   cout>>num2;
-   return 0;
+    int a , b , temp;
+    cout<<"Enter the values :";
+    cin>>a;
+    cin>>b;
+
+    temp = a;
+    a = b;
+    b = temp;
+
+    cout << "\nAfter swapping.";
+    cout << "a = " << a << ", b = " << b;
+
+    return 0;
 }
+ 
