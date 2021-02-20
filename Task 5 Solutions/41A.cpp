d263f26ec3c635ab9baa6ec20424ead2b217bdd5
pskalbhav1 <praseedhakalbhavi@gmail.com>
2021-02-20 14:37:19 +0530

    Create 41A.cpp

---
+++ Task 5 Solutions/41A.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+
+int main()
+{
+    string s,t;
+    cin>>s>>t;
+    reverse(s.begin(),s.end());
+    if(s==t)
+    {
+        cout<<"YES\n";
+    }
+    else
+    {
+        cout<<"NO\n";
+    }
+}
